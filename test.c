b52355d565911f1031c4101fcffa0e049d84fd03
Alex <alex.grushinsky@mail.ru>
2025-03-09 23:26:36 +0300

    добавил тестов

--- test.c
+++ test.c
@@ -650,6 +650,67 @@ START_TEST(s21_sscanf_d2) {
 }
 END_TEST
 
+START_TEST(s21_sscanf_d3) {
+  int s21_res_1, s21_res_2, res_1, res_2;
+  char *s21_arr = " 21 42";
+  char *arr = " 21 42";
+
+  s21_sscanf(s21_arr, "%d%d", &s21_res_1, &s21_res_2);
+  sscanf(arr, "%d%d", &res_1, &res_2);
+
+  ck_assert_int_eq(s21_res_1, res_1);
+  ck_assert_int_eq(s21_res_2, res_2);
+}
+END_TEST
+
+START_TEST(s21_sscanf_d4) {
+  int s21_res_1, res_1;
+  char *s21_arr = "21";
+  char *arr = "21";
+
+  s21_sscanf(s21_arr, "%2d", &s21_res_1);
+  sscanf(arr, "%2d", &res_1);
+
+  ck_assert_int_eq(s21_res_1, res_1);
+}
+END_TEST
+
+START_TEST(s21_sscanf_d5) {
+  int s21_res_1, res_1;
+  char *s21_arr = "+21";
+  char *arr = "+21";
+
+  s21_sscanf(s21_arr, "%2d", &s21_res_1);
+  sscanf(arr, "%2d", &res_1);
+
+  ck_assert_int_eq(s21_res_1, res_1);
+}
+END_TEST
+
+START_TEST(s21_sscanf_d6) {
+  int s21_res_1, res_1;
+  char *s21_arr = "+21";
+  char *arr = "+21";
+
+  s21_sscanf(s21_arr, "%d", &s21_res_1);
+  sscanf(arr, "%d", &res_1);
+
+  ck_assert_int_eq(s21_res_1, res_1);
+}
+END_TEST
+
+START_TEST(s21_sscanf_d7) {
+  int s21_res_1, res_1;
+  char *s21_arr = "-21";
+  char *arr = "-21";
+
+  s21_sscanf(s21_arr, "%d", &s21_res_1);
+  sscanf(arr, "%d", &res_1);
+
+  ck_assert_int_eq(s21_res_1, res_1);
+}
+END_TEST
+
 START_TEST(s21_sscanf_i1) {
   int s21_res_1, s21_res_2, res_1, res_2;
   char *s21_arr = "-21 42";
@@ -728,7 +789,7 @@ START_TEST(s21_sscanf_x1) {
 }
 END_TEST
 
-START_TEST(s21_sscanf_n1) {
+START_TEST(s21_sscanf_n1) {  // дописать сравнение всей фигни и проверить ниже
   int res_n, s21_res_n, num, s21_num;
   char *arr = "21 school";
 
@@ -871,16 +932,83 @@ START_TEST(s21_sscanf_c6) {
   ck_assert_int_eq(s21_res, res);
 }
 END_TEST
-// START_TEST(s21_sscanf_c1) {
-//   int s21_res_1, s21_res_2, res_1, res_2;
-//   char * s21_arr = "0 255";
-//   char * arr = "0 255";
 
-//   s21_sscanf(s21_arr, "%x%x", &s21_res_1, &s21_res_2);
-//   sscanf(arr, "%x%x", &res_1, &res_2);
+START_TEST(s21_sscanf_e1) {
+  char *notation = "-12345e+06";
+  double res, s21_res;
 
-//   ck_assert_int_eq(s21_res_1, res_1);
-//   ck_assert_int_eq(s21_res_2, res_2);
+  s21_sscanf(notation, "%le", &s21_res);
+  sscanf(notation, "%le", &res);
+
+  ck_assert_int_eq(s21_res, res);
+}
+END_TEST
+
+START_TEST(s21_sscanf_e2) {
+  char *notation = "-12345E+06";
+  double res, s21_res;
+
+  s21_sscanf(notation, "%lE", &s21_res);
+  sscanf(notation, "%lE", &res);
+
+  ck_assert_int_eq(s21_res, res);
+}
+END_TEST
+
+START_TEST(s21_sscanf_g1) {
+  char *notation = "12345.6789";
+  double res, s21_res;
+
+  s21_sscanf(notation, "%lg", &s21_res);
+  sscanf(notation, "%lg", &res);
+
+  ck_assert_int_eq(s21_res, res);
+}
+END_TEST
+
+START_TEST(s21_sscanf_g2) {
+  char *notation = "12345.6789";
+  double res, s21_res;
+
+  s21_sscanf(notation, "%lG", &s21_res);
+  sscanf(notation, "%lG", &res);
+
+  ck_assert_int_eq(s21_res, res);
+}
+END_TEST
+
+START_TEST(s21_sscanf_g3) {
+  char *notation = "123e+04";
+  double res, s21_res;
+
+  s21_sscanf(notation, "%lg", &s21_res);
+  sscanf(notation, "%lg", &res);
+
+  ck_assert_int_eq(s21_res, res);
+}
+END_TEST
+
+START_TEST(s21_sscanf_g4) {
+  char *notation = "123E+04";
+  double res, s21_res;
+
+  s21_sscanf(notation, "%lG", &s21_res);
+  sscanf(notation, "%lG", &res);
+
+  ck_assert_int_eq(s21_res, res);
+}
+END_TEST
+
+// START_TEST(s21_sscanf_star1) {
+//   char *str = "*21 school";
+//   int num, num_21;
+//   char *res, *res_21;
+
+//   s21_sscanf(str, "*%d %s", &num_21, res_21);
+//   sscanf(str, "*%d %s", &num, res);
+
+//   ck_assert_int_eq(num, num_21);
+//   ck_assert_str_eq(res, res_21);
 // }
 // END_TEST
 
@@ -966,6 +1094,11 @@ int main(void) {
   // sscanf
   tcase_add_test(tc1_1, s21_sscanf_d1);
   tcase_add_test(tc1_1, s21_sscanf_d2);
+  tcase_add_test(tc1_1, s21_sscanf_d3);
+  tcase_add_test(tc1_1, s21_sscanf_d4);
+  tcase_add_test(tc1_1, s21_sscanf_d5);
+  tcase_add_test(tc1_1, s21_sscanf_d6);
+  tcase_add_test(tc1_1, s21_sscanf_d7);
   tcase_add_test(tc1_1, s21_sscanf_i1);
   tcase_add_test(tc1_1, s21_sscanf_f1);
   tcase_add_test(tc1_1, s21_sscanf_u1);
@@ -984,6 +1117,15 @@ int main(void) {
   tcase_add_test(tc1_1, s21_sscanf_c4);
   tcase_add_test(tc1_1, s21_sscanf_c5);
   tcase_add_test(tc1_1, s21_sscanf_c6);
+  tcase_add_test(tc1_1, s21_sscanf_e1);
+  tcase_add_test(tc1_1, s21_sscanf_e2);
+  tcase_add_test(tc1_1, s21_sscanf_g1);
+  tcase_add_test(tc1_1, s21_sscanf_g2);
+  tcase_add_test(tc1_1, s21_sscanf_g3);
+  tcase_add_test(tc1_1, s21_sscanf_g4);
+  // tcase_add_test(tc1_1, s21_sscanf_star1);
+
+
 
   srunner_set_fork_status(sr, CK_NOFORK);
   srunner_run_all(sr, CK_ENV);
